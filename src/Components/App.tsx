8ccdeae11061074c3beb9c54651b4d678ec46a23
KeuzNub <breixopastoriza@gmail.com>
2024-11-10 12:53:26 +0100

    First Commit

---
+++ src/Components/App.tsx
@@ -0,0 +1,24 @@
+
+import '../App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='rounded-full shadow border-2 bg-white text-center'>
+        <h1 className='font-serif text-3xl '>Hola mundo</h1>
+        <div>
+            <h2>hola</h2>
+            <h2>hola</h2>
+            <ul>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+            </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
